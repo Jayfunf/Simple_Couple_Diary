e688573a285cb68f7ee6f2130d12885014f35565
Jayfunf <simh3077@ajou.ac.kr>
2023-01-09 22:39:42 +0900

    init

---
+++ Simple_Couple_Diary/Simple_Couple_Diary/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Simple_Couple_Diary
+//
+//  Created by Minhyun Cho on 2023/01/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
